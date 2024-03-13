f14d7cc7b5b8643658eef0a8e14ac8da487cfe37
chansm@mz.co.kr <chanyoung3320@gmail.com>
2024-03-13 15:55:06 +0900

    testt

---
+++ components/Qr.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { QrcodeStream } from "vue-qrcode-reader";
+
+const data = reactive({
+  paused: false,
+  result: "",
+  showScanConfirmation: false,
+  camera: "off",
+});
+const active = ref(false);
+
+const onCameraOn = () => {
+  data.showScanConfirmation = false;
+};
+
+const onCameraOff = () => {
+  data.showScanConfirmation = true;
+};
+
+const onError = (error) => {
+  console.error(error);
+};
+const activate = async () => {
+  active.value = true;
+  setTimeout(() => {
+    active.value = false;
+  }, 50);
+};
+
+const onDecode = async () => {
+  data.camera = `off`;
+  //Validate code
+  data.camera = `auto`;
+  activate();
+};
+const onDetect = async (detectedCodes) => {
+  data.result = JSON.stringify(detectedCodes.map((code) => code.rawValue));
+
+  data.paused = true;
+  await timeout(500);
+  data.paused = false;
+};
+
+const timeout = (ms) => {
+  return new Promise((resolve) => {
+    window.setTimeout(resolve, ms);
+  });
+};
+</script>
+<template>
+  <div>
+    <div v-if="active"></div>
+
+    <QrcodeStream
+      v-else
+      :paused="data.paused"
+      @decode="onDecode"
+      @detect="onDetect"
+      @camera-on="onCameraOn"
+      @camera-off="onCameraOff"
+      @error="onError"
+    >
+      <div v-show="data.showScanConfirmation" class="scan-confirmation">
+        <img :src="'/checkmark.svg'" alt="Checkmark" width="128" />
+      </div>
+    </QrcodeStream>
+  </div>
+</template>
+
+<style scoped>
+.scan-confirmation {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba(255, 255, 255, 0.8);
+
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-const counter = useState('counter', () => Math.round(Math.random() * 1000));
+const counter = useState("counter", () => Math.round(Math.random() * 1000));
 </script>
 
 <template>
   <div>
     <NuxtLink to="/products">VueUse Pratice</NuxtLink>
     <NuxtLink to="/pinia">pinia Pratice</NuxtLink>
+    <Qr />
   </div>
 </template>
 
